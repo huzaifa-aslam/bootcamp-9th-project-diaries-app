9eb4c5ea18641f3f6b2c345926659374e23c2dca
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-10-04 01:27:28 +0500

    finished function part

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {setupServer} from './services/mirage/server'
+import store from './store'
+import {Provider} from 'react-redux'
 if (process.env.NODE_ENV === 'development') {
   setupServer();
 }
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
