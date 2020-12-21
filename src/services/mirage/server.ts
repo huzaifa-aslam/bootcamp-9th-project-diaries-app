8942f838ac6161d96488f2f1fe7e5204f972b15c
huzaifa aslam <huzaifaslam45@gmail.com>
2020-12-21 12:04:38 +0500

    added functionalities for user and diary

---
+++ src/services/mirage/server.ts
@@ -0,0 +1,66 @@
+import {Server, Model, Factory, belongsTo, hasMany, Response} from 'miragejs';
+import user from './routes/user';
+import * as diary from './routes/diary'
+
+export const handleErrors = (error: any, message = 'An error ocurred')=>{
+    return new Response(400, undefined, {
+        data: {
+            message,
+            isError: true
+        },
+    });
+};
+
+export const setupServer=(env?: string): Server => {
+    return new Server({
+        environment: env && 'development',
+
+        models: {
+            entry: Model.extend({
+                diary: belongsTo(),
+            }),
+            diary: Model.extend({
+                entry: hasMany(),
+                user: belongsTo(),
+            }),
+            user: Model.extend({
+                diary: hasMany(),
+            }),
+        },
+
+        factories: {
+            user: Factory.extend({
+                username: 'Huzaifa Aslam',
+                password: 'admin123',
+                email: 'huzaifa@gmail.com'
+            })
+        },
+
+        seeds: (server): any =>{
+            server.create('user')
+        },
+
+        routes(): void {
+            this.urlPrefix = `http://localhost:3000`;
+
+            this.get('/diaries/entries/:id',diary.getEntries);
+            this.get('/diaries/:id',diary.getDiaries);
+
+            this.post('/auth/login',user.login);
+            this.post('/auth/signup',user.signup);
+
+            this.post('/diary/',diary.create);
+            this.post('/diary/entry/:id',diary.addEntry);
+
+            this.put('/diaries/entry/:id',diary.updateEntry);
+            this.put('/diaries/:id',diary.updateDiary);
+
+
+
+        },
+
+        
+
+
+    });
+};
